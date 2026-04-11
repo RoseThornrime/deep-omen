933562d315c51d4ed2195c9beb9e451e5064f878
PSielchan <147639332+PSielchan@users.noreply.github.com>
2026-04-11 17:32:19 +0200

    hardcoding dialogs 1

---
+++ scripts/dialogs.gd
@@ -0,0 +1,27 @@
+extends Sprite2D
+
+@onready var question_buble: Sprite2D = $Question
+@onready var q_label: Label = $Question/Label
+@onready var answer_buble: TextureButton = $Answer
+@onready var a1_label: Label = $Answer/Label
+@onready var answer2_buble: TextureButton = $Answer2
+@onready var a2_label: Label = $Answer2/Label
+
+func guest(tekst):
+	q_label.text=tekst
+	question_buble.show()
+
+func pc(tekst):
+	pass
+	
+func a1(tekst):
+	a1_label.text=tekst
+	answer_buble.show()
+	
+func a2(tekst):
+	a2_label.text=tekst
+	answer2_buble.show()
+
+func story0():
+	guest("wiedźmo morska! jestem skończony. nie wiem jaka mnie przyszłość czeka. umiesz przepowiadać? pomórz mi")
+	
